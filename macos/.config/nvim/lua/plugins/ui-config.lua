e14642879f0e9bee2efc8ffc2907142f44772b3d
Sejong Heo <tromberx@gmail.com>
2025-08-02 10:29:05 +0900

    feat(macos): add comment and file-movement plugins for nvim

--- macos/.config/nvim/lua/plugins/ui-config.lua
+++ macos/.config/nvim/lua/plugins/ui-config.lua
@@ -2,9 +2,51 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    ---@module "ibl"
+    -- -@module "ibl"
     ---@type ibl.config
     opts = {},
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      require("telescope").load_extension("harpoon")
+      harpoon:setup()
+      -- 파일 추가
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end)
+      -- 빠른 메뉴 (Harpoon UI)
+      vim.keymap.set("n", "<C-m>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      -- 파일 간 빠른 이동
+      vim.keymap.set("n", "<leader>1", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>2", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>3", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>4", function()
+        harpoon:list():select(4)
+      end)
+
+      -- Telescope 연동 보기
+      vim.keymap.set("n", "<leader>hf", "<cmd>Telescope harpoon marks<cr>")
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("Comment").setup()
+    end,
   },
   {
     "Djancyp/better-comments.nvim",
