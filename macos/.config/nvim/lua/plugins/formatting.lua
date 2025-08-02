6434bdd99df6f4d65f2a28a3d2c5138026109e9a
Sejong Heo <tromberx@gmail.com>
2025-08-02 21:28:43 +0900

    feat(macos): convert none-ls to nvim-lint and conform

---
+++ macos/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      notify_on_error = true,
+      formatters_by_ft = {
+        -- Conform will run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        -- rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+
+        lua = { "stylua" },
+        -- Web
+        html = { "prettierd", "djlint" },
+        htmldjango = { "djlint" },
+        jinja = { "djlint" },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        css = { "prettierd" },
+
+        -- Markup
+        yaml = { "prettierd" },
+        json = { "prettierd" },
+
+        -- Markdown
+        markdown = { "prettierd", "markdownlint-cli2", "markdown-toc" },
+
+        -- C/C++
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        cmake = { "cmake_format" },
+        -- Python
+        python = { "ruff_format", "ruff_organize_imports", "ruff_fix" },
+        -- Rust
+        rust = { "rustfmt" },
+      },
+      formatters = {
+        djlint = { prepend_args = { "--profile=jinja" } },
+        -- cmake_format = { args = { "--space=4" } },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        async = false,
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+      keys = {
+        {
+          "<leader>cf",
+          function()
+            require("conform").format({ async = false, timeout_ms = 500, lsp_format = "fallback" })
+          end,
+          mode = "",
+          desc = "[C]ode [F]ormat",
+        },
+      },
+    },
+  },
+}
