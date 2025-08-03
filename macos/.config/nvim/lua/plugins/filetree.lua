af11bef0497fb3057a28a38c4c413de05745d34f
Sejong Heo <tromberx@gmail.com>
2025-08-03 21:05:10 +0900

    feat(macos): nvim's filetree
    - add oil.nvim

---
+++ macos/.config/nvim/lua/plugins/filetree.lua
@@ -0,0 +1,76 @@
+return {
+  -- Neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      -- fill any relevant options here
+      filesystem = {
+        filtered_items = {
+          -- visible = true,
+          hide_dotfiles = false,
+          -- hide_gitignored = true,
+        },
+      },
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+    },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+    end,
+  },
+  -- Oil
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        -- oil 버퍼에서 사용될 keymap 설정
+        ["c"] = { mode = "n" },
+      },
+      float = {
+        preview_split = "below",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function(_, opts)
+      local oil = require("oil")
+      oil.setup(opts)
+      vim.keymap.set("n", "-", oil.toggle_float, {})
+      -- oil 버퍼에서만 적용되는 매핑 설정 (nvim_create_autocmd 사용 권장)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "oil",
+        callback = function()
+          -- vim.keymap.set("n", "C", function()
+          --   require("oil").open_new_file()
+          -- end, { desc = "Create file/directory" })
+          -- vim.keymap.set("n", "R", function()
+          --   require("oil").rename_entry()
+          -- end, { desc = "Rename file/directory" })
+          -- vim.keymap.set("n", "D", function()
+          --   require("oil").delete_entry()
+          -- end, { desc = "Delete file/directory" })
+        end,
+      })
+    end,
+  },
+}
