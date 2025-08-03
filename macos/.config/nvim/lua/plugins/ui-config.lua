1fab57ea794abeb83e786631206f20d96a6deaf0
Sejong Heo <tromberx@gmail.com>
2025-08-03 20:59:00 +0900

    feat(macos): update nvim's ui-config.lua

--- macos/.config/nvim/lua/plugins/ui-config.lua
+++ macos/.config/nvim/lua/plugins/ui-config.lua
@@ -1,110 +1,159 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    -- -@module "ibl"
-    ---@type ibl.config
-    opts = {},
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {},
-  },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    config = function()
-      local harpoon = require("harpoon")
-      require("telescope").load_extension("harpoon")
-      harpoon:setup()
-      -- 파일 추가
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():add()
-      end)
-      -- 빠른 메뉴 (Harpoon UI)
-      vim.keymap.set("n", "<C-m>", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
-
-      -- 파일 간 빠른 이동
-      vim.keymap.set("n", "<leader>1", function()
-        harpoon:list():select(1)
-      end)
-      vim.keymap.set("n", "<leader>2", function()
-        harpoon:list():select(2)
-      end)
-      vim.keymap.set("n", "<leader>3", function()
-        harpoon:list():select(3)
-      end)
-      vim.keymap.set("n", "<leader>4", function()
-        harpoon:list():select(4)
-      end)
-
-      -- Telescope 연동 보기
-      vim.keymap.set("n", "<leader>hf", "<cmd>Telescope harpoon marks<cr>")
-    end,
-  },
-  {
-    "numToStr/Comment.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("Comment").setup()
-    end,
-  },
-  {
-    "Djancyp/better-comments.nvim",
-    config = function()
-      require("better-comment").Setup({
-        tags = {
-          {
-            name = "TODO",
-            fg = "white",
-            bg = "#0a7aca",
-            bold = true,
-            virtual_text = "",
-          },
-          {
-            name = "FIX",
-            fg = "white",
-            bg = "#f44747",
-            bold = true,
-            virtual_text = "",
-          },
-          {
-            name = "WARNING",
-            fg = "#FFA500",
-            bg = "",
-            bold = false,
-            virtual_text = "",
-          },
-          {
-            name = "ERROR",
-            fg = "#f44747",
-            bg = "",
-            bold = true,
-            virtual_text = " ",
-          },
-          {
-            name = "INFO",
-            fg = "#3498DB",
-            bg = "",
-            bold = true,
-            virtual_text = " ",
-          },
-          {
-            name = "REMARK",
-            fg = "#98C379",
-            bg = "",
-            bold = true,
-            virtual_text = " ",
-          },
-        },
-      })
-    end,
-  },
   -- {
-  -- 	"folke/todo-comments.nvim",
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- add any options here
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     "MunifTanjim/nui.nvim",
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     "rcarriga/nvim-notify",
+  --   },
   -- },
+  -- {
+  --   "goolord/alpha-nvim",
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --
+  --   config = function()
+  --     local alpha = require("alpha")
+  --     local dashboard = require("alpha.themes.startify")
+  --
+  --     dashboard.section.header.val = {
+  --       "                                                     ",
+  --       "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  --       "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  --       "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  --       "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  --       "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  --       "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  --       "                                                     ",
+  --     }
+  --
+  --     alpha.setup(dashboard.opts)
+  --   end,
+  -- },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    dependencies = {
+      "echasnovski/mini.icons",
+    },
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      dashboard = {
+        enabled = true,
+        preset = {
+          header = [[
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+         ]],
+        },
+      },
+      -- indent = { enabled = true }, -- use treesitter indent
+      input = { enabled = true },
+      git = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+    keys = {
+      {
+        "<leader>sf",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>S",
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer",
+      },
+      {
+        "<leader>gl",
+        function()
+          Snacks.lazygit.log_file()
+        end,
+        desc = "Lazygit Log (cwd)",
+      },
+      {
+        "<leader>lg",
+        function()
+          Snacks.lazygit()
+        end,
+        desc = "Lazygit",
+      },
+      -- {
+      --   "<C-p>",
+      --   function()
+      --     Snacks.picker.pick("files")
+      --   end,
+      --   desc = "Find Files",
+      -- },
+      -- {
+      --   "<leader><leader>",
+      --   function()
+      --     Snacks.picker.recent()
+      --   end,
+      --   desc = "Recent Files",
+      -- },
+      -- {
+      --   "<leader>fb",
+      --   function()
+      --     Snacks.picker.buffers()
+      --   end,
+      --   desc = "Buffers",
+      -- },
+      -- {
+      --   "<leader>fg",
+      --   function()
+      --     Snacks.picker.grep()
+      --   end,
+      --   desc = "Grep Files",
+      -- },
+      -- {
+      --   "<C-n>",
+      --   function()
+      --     Snacks.explorer()
+      --   end,
+      --   desc = "Explorer",
+      -- },
+    },
+  },
 }
