3def8ff060caacf6c495deaeabf8bd4d43818241
Sejong Heo <tromberx@gmail.com>
2025-08-06 21:49:54 +0900

    feat(macos): add nvim-ufo.nvim for code-folding

--- macos/.config/nvim/lua/plugins/editting.lua
+++ macos/.config/nvim/lua/plugins/editting.lua
@@ -15,6 +15,25 @@ return {
     opts = {},
   },
   {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "lsp", "indent" }
+        end,
+      })
+    end,
+  },
+  {
     "tpope/vim-sleuth",
     -- No further initialization needed, as this is a real "vim" not a lua
     -- plugin.
