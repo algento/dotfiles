e17b5d6a76448cd9184459eed0663c5a25ff4843
Sejong Heo <tromberx@gmail.com>
2025-10-09 15:26:45 +0900

    feat(macos): add yazi bookmark plugin

--- macos/.config/yazi/init.lua
+++ macos/.config/yazi/init.lua
@@ -16,5 +16,24 @@ require("full-border"):setup({
   -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
   type = ui.Border.ROUNDED,
 })
-require("smart-enter"):setup {
-}
+require("smart-enter"):setup({})
+
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+  last_directory = { enable = false, persist = false, mode = "dir" },
+  -- persist = "none",
+  persist = "vim",
+  desc_format = "full",
+  file_pick_mode = "hover",
+  custom_desc_input = false,
+  show_keys = true,
+  notify = {
+    enable = false,
+    timeout = 1,
+    message = {
+      new = "New bookmark '<key>' -> '<folder>'",
+      delete = "Deleted bookmark in '<key>'",
+      delete_all = "Deleted all bookmarks",
+    },
+  },
+})
